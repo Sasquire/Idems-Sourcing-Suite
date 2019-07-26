6b7eef449e75b70cb59c433d97196336f9b40da3
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-07-26 01:41:03 -0400

    pixiv description

--- source/pixiv.js
+++ source/pixiv.js
@@ -9,32 +9,47 @@ async function pixiv(){
 
 	// Figure containing images
 	await wait_for_query('main figure > div[role="presentation"]');
+	setup_description();
 
 	const button = $qa('button').find(e => e.innerText.includes('See all'));
 	if(button !== undefined){
 		// https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
 		await wait_for_query('#\\32 '); // Wait for images
 	}
-	// A await new Promise(resolve => setTimeout(resolve, 100));
+	add_md5s();
 
-	let i = 1;
-	// eslint-disable-next-line max-len
-	const images = $qa('main div[role="presentation"] > a');
-	for(const image of images){
-		console.log(`Trying image ${i++}`);
 
-		const node = image.parentNode.parentNode;
-		const full_url = image.href;
-		const preview_url = full_url
-			.replace('img-original', 'img-master')
-			.replace(/.(png|jpg)$/u, '_master1200.jpg');
+	async function add_md5s(){
+		let i = 1;
+		// eslint-disable-next-line max-len
+		const images = $qa('main div[role="presentation"] > a');
+		for(const image of images){
+			console.log(`Trying image ${i++}`);
 
-		const hash_data = await Promise.all([
-			add_md5(full_url, 'full', {referer: window.location.href}),
-			add_md5(preview_url, 'sample', {referer: window.location.href})
-		]);
-		const html = pretty_md5(hash_data);
-		html.classList.add('md5_hash');
-		node.appendChild(html);
+			const node = image.parentNode.parentNode;
+			const full_url = image.href;
+			const preview_url = full_url
+				.replace('img-original', 'img-master')
+				.replace(/.(png|jpg)$/u, '_master1200.jpg');
+
+			const hash_data = await Promise.all([
+				add_md5(full_url, 'full', {referer: window.location.href}),
+				add_md5(preview_url, 'sample', {referer: window.location.href})
+			]);
+			const html = pretty_md5(hash_data);
+			html.classList.add('md5_hash');
+			node.appendChild(html);
+		}
+	}
+
+	function setup_description(){
+		const box = $q('figcaption');
+		const title = $q('h1', box).textContent;
+		const artist = $q('aside > section > h2 > div > div').textContent;
+		description_button(
+			$q('footer', box), // Where to place description
+			$q(' div ~ div ~ div', box), // Description node
+			`${title} - by ${artist}` // Title
+		);
 	}
 }
