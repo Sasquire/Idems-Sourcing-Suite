1388ed42b7911a9330b28def7ef085892c7b594b
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-08-06 20:24:46 -0400

    Fixed bvas to actually run
    I just didn't run the function, how could I be so blind.

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Idem's Sourcing Suite local
 // @description Adds a whole bunch of utilities, helpful for sourcing images
-// @version     1.00014
+// @version     1.00015
 // @author      Meras
 
 // @namespace   https://github.com/Sasquire/
